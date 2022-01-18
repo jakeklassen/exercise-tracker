ccb7c1e1c70e017fc35a6b7c7bf8f68b64202722
Jake Klassen <jklassendev@gmail.com>
2022-01-17 21:03:09 -0500

    some logging

--- src/providers/mongoose.ts
+++ src/providers/mongoose.ts
@@ -8,6 +8,9 @@ declare module '#app/container.js' {
 }
 
 export const resolveDatabaseConnection = async ({ config }: AppCradle) => {
+  console.log(config.get('mongoUrl'));
+  console.log(config.get('mongoDb'));
+
   await mongoose.connect(config.get('mongoUrl'), {
     autoIndex: false,
     dbName: config.get('mongoDb'),

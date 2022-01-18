d360d17c07be52c293ba742e29d95b68f9c73a99
Jake Klassen <jklassendev@gmail.com>
2022-01-18 16:41:11 -0500

    remove pg test

--- src/connect-test.ts
+++ src/connect-test.ts
@@ -1,20 +1,4 @@
 import mongoose from 'mongoose';
-import pg from 'pg';
-
-const { Client } = pg;
-
-const client = new Client({
-  host: 'postgres',
-  user: 'sidequest',
-  password: 'elephant',
-  database: 'sidequest',
-  port: 5432,
-});
-await client.connect();
-console.log('Connected to Postgres');
-
-await client.query('SELECT NOW()');
-await client.end();
 
 const connection = await mongoose
   .createConnection('mongodb://mongo:27017', {
